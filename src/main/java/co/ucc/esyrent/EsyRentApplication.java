d9d838f668e47352edd57dbc3ef5ef931008f889
Jose B <joseluisburbano19105@gmail.com>
2026-05-22 13:30:23 -0500

    first commit

---
+++ src/main/java/co/ucc/esyrent/EsyRentApplication.java
@@ -0,0 +1,13 @@
+package co.ucc.esyrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsyRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsyRentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/ucc/esyrent/EsyRentApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucc.esyrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsyRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
